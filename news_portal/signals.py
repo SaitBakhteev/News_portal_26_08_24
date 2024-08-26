e7abeb4fd1f3accb9dc73d2ec5e7d19fbea3b9bf
SaitBakhteev <148718664+SaitBakhteev@users.noreply.github.com>
2024-08-27 00:40:52 +0300

    Update signals.py

--- news_portal/signals.py
+++ news_portal/signals.py
@@ -6,11 +6,7 @@ from django.dispatch import receiver
 from django.conf import settings
 from .models import Post, PostCategory
 
-# @receiver(signal=m2m_changed, sender=PostCategory)
-# def notify_pre_create (sender, instance,**kwargs):
-#       if kwargs['action'] == 'post_add':
-#             print(f'PostCategory changed')
-
-# @receiver(signal=post_save, sender=Post)
-# def notify_post_save (sender,instance,**kwargs):
-#       print(f'This is post_save. Short: {instance}')
+@receiver(signal=m2m_changed, sender=PostCategory)
+def notify_pre_create (sender, instance,**kwargs):
+      if kwargs['action'] == 'post_add':
+            print(f'PostCategory changed')
